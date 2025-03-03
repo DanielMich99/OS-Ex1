712f821a140bbe6db6464015ca3280295abcdbfd
DanielMich99 <liatm1412@gmail.com>
2025-03-03 17:21:38 +0200

    Initial commit - added relevant files

--- myCommands/gccfind.sh
+++ myCommands/gccfind.sh
@@ -28,11 +28,41 @@ gcc -w -o ${file%.c}.out $file
 done
 
 
+#!/bin/bash
+#Daniel Michaelshvili 207795030
+
+#check if the number of arguments is below 2
+if [ $# -lt 2 ]
+then
+echo "Not enough parameters"
+exit 1
+fi
+
+# simple check if $1 is a valid path
+if [ ! -d $1 ]
+then
+exit 1
+fi
+
+#cd $1
+
+# remove all the ".out" files in the $1 not recursive with rm
+find $1 -maxdepth 1 -type f -name "*.out" -exec rm {} \;
+
+# # # for loop that goes through all the .c files in the directory without subdirectories and compiles them if they have the $2 word in them with no sense of case, with the name of the file.out
+for file in $(find $1 -maxdepth 1 -type f -name "*.c" -exec grep -qwi "\b$2\b" {} \; -print )
+do
+# not include the last 2 characters of the file name (the .c extension)
+gcc -w -o ${file%.c}.out $file
+done
+
 if [[ "$3" == "-r" ]]
 then
-  for dir in `find $1 -maxdepth 1 -wholename "${1}/*" -type d`
+  for dir in "$1"/*/
   do
-    $0 $dir $2 "-r"
+    if [ -d "$dir" ]; then
+      $0 $dir $2 "-r"
+    fi
   done
 fi
 
--- myCommands/tennis.sh
+++ myCommands/tennis.sh
@@ -3,29 +3,28 @@
 
 #
 #variables
-p1_win="PLAYER 1 WINS !"
-p2_win="PLAYER 2 WINS !"
-play=true
-declare -i p1Points=50
-declare -i p2Points=50
+
+gameOn=true
 declare -i p1Choice=0
 declare -i p2Choice=0
+declare -i p1Points=50
+declare -i p2Points=50
 declare -i ballPosition=0
 
 
 #print points left
-points_left() {
+pointsLeft() {
     echo -e " Player 1: ${1}         Player 2: ${2} "
 }
 
 
 #print last play
-points_played() {
+pointsPlayed() {
     echo -e "       Player 1 played: ${1}\n       Player 2 played: ${2}\n\n"
 }
 
 
-draw_ball() {
+drawBall() {
     case $1 in 
 
     -3)
@@ -59,30 +58,30 @@ draw_ball() {
 
 # create board
 createBoard() {
-    points_left $1 $2
-    echo "----------------------------------"
+    pointsLeft $1 $2
+    echo "---------------------------------"
     echo " |       |       #       |       | "
     echo " |       |       #       |       | "
-    draw_ball $3
+    drawBall $3
     echo " |       |       #       |       | "
     echo " |       |       #       |       | "
-    echo "----------------------------------"
+    echo "---------------------------------"
     case $4 in
     false)
         #do nothing
         ;;
     true)
-        points_played $5 $6
+        pointsPlayed $5 $6
         ;;
     esac
 
 }
 
 #get points from user
-get_points() {
+getPoints() {
     re='^[0-9]+$' 
     waiting_for_number=true
-    invalid="NOT A VALID MOVE !"
+
     # while waiting for a valid is true
     while [ "$waiting_for_number" = true ]; do
         echo "PLAYER ${1} PICK A NUMBER: "
@@ -98,13 +97,13 @@ get_points() {
             esac
             waiting_for_number=false
         else
-            echo $invalid
+            echo "NOT A VALID MOVE !"
         fi
     done
 }
 
 #move the ball acording to the points
-move_ball() {
+moveBall() {
     #if player 1 won the move
     if [ $1 -gt $2 ]; then
         if [ $ballPosition -lt 0 ]; then
@@ -129,7 +128,7 @@ move_ball() {
 }
 
 #update points
-update_points() {
+updatePoints() {
     p1Points=$((p1Points - p1_serve))
     p2Points=$((p2Points - p2_serve))
 }
@@ -139,46 +138,50 @@ gameOverCheck() {
     #if the ball is at the end of the right side of the board player 1 wins the game,
     #else if the ball is at the end of the left side of the board player 2 wins the game.
     if [ $ballPosition -eq 3 ]; then
-        echo $p1_win
-        play=false
+        echo "PLAYER 1 WINS !"
+        gameOn=false
+        return
     elif [ $ballPosition -eq -3 ]; then
-        echo $p2_win
-        play=false
+        echo "PLAYER 2 WINS !"
+        gameOn=false
+        return
     fi
 
     #if player 1 has points and player 2 doesn't have points player 1 wins the game, else if player 2 has points and player 1 doesn't have points player 2 wins the game.
     if [ $p1Points -gt 0 ] && [ $p2Points -eq 0 ]; then
-        echo $p1_win
-        play=false
+        echo "PLAYER 1 WINS !"
+        gameOn=false
+        return
     elif [ $p2Points -gt 0 ] && [ $p1Points -eq 0 ]; then
-        echo $p2_win
-        play=false
+        echo "PLAYER 2 WINS !"
+        gameOn=false
+        return
     fi
 
     #if both players have 0 points and the ball position is on the right side of the board player 1 wins the game,
     #if both players have 0 points and the ball position is on the left side of the board player 2 wins the game,
     #else if both players have 0 points and the ball position is in the middle of the board the game is a draw.
     if [ $p1Points -eq 0 ] && [ $p2Points -eq 0 ] && [ $ballPosition -gt 0 ]; then
-        echo $p1_win
-        play=false
+        echo "PLAYER 1 WINS !"
+        gameOn=false
     elif [ $p1Points -eq 0 ] && [ $p2Points -eq 0 ] && [ $ballPosition -lt 0 ]; then
-        echo $p2_win
-        play=false
+        echo "PLAYER 2 WINS !"
+        gameOn=false
     elif [ $p1Points -eq 0 ] && [ $p2Points -eq 0 ] && [ $ballPosition -eq 0 ]; then
         echo "IT'S A DRAW !"
-        play=false
+        gameOn=false
     fi 
 }
 
 #main loop
 createBoard $p1Points $p2Points $ballPosition false
-while [ "$play" = true ]; do
+while [ "$gameOn" = true ]; do
     #clear
     #createBoard $p1Points $p2Points $ballPosition false
-    get_points 1 $p1Points
-    get_points 2 $p2Points
-    move_ball $p1_serve $p2_serve
-    update_points
+    getPoints 1 $p1Points
+    getPoints 2 $p2Points
+    moveBall $p1_serve $p2_serve
+    updatePoints
     createBoard $p1Points $p2Points $ballPosition true $p1_serve $p2_serve
     gameOverCheck
 done
